65c4c83dd1126b9959d6d6c134b61441df78e04e
Graham Sellers <graham@openglsuperbible.com>
2013-11-05 16:26:01 -0500

    Drop shader version to 410 for wrapmodes sample for Mac build.

--- src/wrapmodes/wrapmodes.cpp
+++ src/wrapmodes/wrapmodes.cpp
@@ -26,7 +26,7 @@
 
 static const char * vs_source[] =
 {
-    "#version 420 core                                                              \n"
+    "#version 410 core                                                              \n"
     "                                                                               \n"
     "uniform vec2 offset;                                                           \n"
     "                                                                               \n"
@@ -46,7 +46,7 @@ static const char * vs_source[] =
 
 static const char * fs_source[] =
 {
-    "#version 430 core                                                              \n"
+    "#version 410 core                                                              \n"
     "                                                                               \n"
     "uniform sampler2D s;                                                           \n"
     "                                                                               \n"
