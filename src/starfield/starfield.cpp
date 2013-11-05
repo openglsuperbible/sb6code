ffa6d8e1c038ce8d000193c0aeeb9c016822f24a
Graham Sellers <graham@openglsuperbible.com>
2013-11-05 16:30:46 -0500

    Drop shader version of starfield example to 410 for Mac build.

--- src/starfield/starfield.cpp
+++ src/starfield/starfield.cpp
@@ -66,7 +66,7 @@ class starfield_app : public sb6::application
 
         static const char * fs_source[] =
         {
-            "#version 420 core                                              \n"
+            "#version 410 core                                              \n"
             "                                                               \n"
             "layout (location = 0) out vec4 color;                          \n"
             "                                                               \n"
@@ -81,7 +81,7 @@ class starfield_app : public sb6::application
 
         static const char * vs_source[] =
         {
-            "#version 420 core                                              \n"
+            "#version 410 core                                              \n"
             "                                                               \n"
             "layout (location = 0) in vec4 position;                        \n"
             "layout (location = 1) in vec4 color;                           \n"
